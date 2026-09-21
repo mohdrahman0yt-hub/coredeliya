3442e7e4659850a0fa7b9b5ae103c5d5f9c6961b
EKHLAKU-RAHMAN <mdrahman773958@gmail.com>
2026-09-21 19:15:04 +0530

    add

--- src/components/MobileStickyCTA.jsx
+++ src/components/MobileStickyCTA.jsx
@@ -28,7 +28,7 @@ const MobileStickyCTA = () => {
         className="mobile-sticky-cta-button mobile-sticky-connect"
         href={CALL_NUMBER}
         onClick={startCall}
-        aria-label="Call +91 9402325828"
+        aria-label="Call 9402325828"
       >
         <Phone size={18} strokeWidth={2.3} />
         Connect
