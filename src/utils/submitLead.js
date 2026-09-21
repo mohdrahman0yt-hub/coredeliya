7032fd6ae6033b21dc8ab73003f1d2e70c57846c
EKHLAKU-RAHMAN <mdrahman773958@gmail.com>
2026-09-21 16:29:36 +0530

    error solve

--- src/utils/submitLead.js
+++ src/utils/submitLead.js
@@ -1,6 +1,6 @@
 const GOOGLE_SCRIPT_URL =
-  'https://script.google.com/macros/s/AKfycbxV3cQxYD1mIbjy7NE5_qDmtTX5eLajpc6Z8pWCKUr9yLzj6mhtX5eAHCfWmxhCuxdA1g/exec'
-
+  // 'https://script.google.com/macros/s/AKfycbxV3cQxYD1mIbjy7NE5_qDmtTX5eLajpc6Z8pWCKUr9yLzj6mhtX5eAHCfWmxhCuxdA1g/exec'
+  'https://script.google.com/macros/s/AKfycbz2UIESoamR47iHIuH1WKQdGvNHyrF4veN5i6BKwjkjLcyg0eaWyp3muofOQnV8zE7P4A/exec'
 export async function submitLead({ destination, name, email, phone }) {
   if (!GOOGLE_SCRIPT_URL) {
     throw new Error('Google Apps Script Web App URL is not configured yet.')
